fd4e617cded8f9c9ae62df97e5958be1209c0322
J.Fırat <61869832+JFASOF@users.noreply.github.com>
2021-03-07 16:06:29 +0300

    Update konum_bul.py

--- konum_bul.py
+++ konum_bul.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-send_url = "http://api.ipstack.com/check?access_key=7bf70b206feb1b82eca4a6195331e493"
+send_url = "http://api.ipstack.com/check?access_key='your-api-key'"
 geo_req = requests.get(send_url)
 geo_json = json.loads(geo_req.text)
 latitude = geo_json['latitude']
